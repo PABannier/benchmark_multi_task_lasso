ae458e86c8b9b15c97dc2053670fb9db8173bab9
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-07-13 20:56:04 +0200

    setting hard on real data

--- objective.py
+++ objective.py
@@ -10,7 +10,7 @@ class Objective(BaseObjective):
     name = "Objective"
     # parameters = {"reg": [.5], "n_orient": [1, 3]}
     # parameters = {"reg": [.5], "n_orient": [1]}
-    parameters = {"reg": [.5], "n_orient": [3]}
+    parameters = {"reg": [.1], "n_orient": [3]}
 
     def __init__(self, reg=0.1, n_orient=1):
         self.reg = reg
