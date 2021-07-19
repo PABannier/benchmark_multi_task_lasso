0ef211ee01bfd1721d55b636a8dfe42cfaf0f0d1
Pierre-Antoine Bannier <pierre-antoine@mbpdepieantoine.home>
2021-07-19 17:20:45 +0200

    FIX partial fix of mne solver

--- solvers/bcd_mne.py
+++ solvers/bcd_mne.py
@@ -1,9 +1,12 @@
+from mtl_utils.common import norm_l2inf
 from benchopt import BaseSolver
 from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
+    import numpy as np
     from mne.inverse_sparse.mxne_inverse import mixed_norm_solver
-    from mtl_utils.common import build_full_coefficient_matrix, get_alpha_max
+    from mtl_utils.common import (build_full_coefficient_matrix, get_alpha_max,
+                                 sum_squared)
 
 
 class Solver(BaseSolver):
@@ -12,15 +15,22 @@ class Solver(BaseSolver):
 
     def set_objective(self, X, Y, lmbd, n_orient):
         self.X, self.Y = X, Y
-        # Rescale alpha to be in [0, 100)
-        self.lmbd = 100 * lmbd / get_alpha_max(X, Y, n_orient)
+        self.lmbd = lmbd
         self.n_orient = n_orient
         self.maxit = 100_000
-        self.tol = 1e-12
+        self.tol = 1e-8 * sum_squared(self.Y)
+
+        # Rescale alpha to be in [0, 100)
+        self.lmbd_max = norm_l2inf(np.dot(X.T, Y), n_orient, copy=False)
+        self.lmbd_max *= 0.01
+        self.X /= self.lmbd_max
 
     def run(self, n_iter):
-        W, as_, _ = mixed_norm_solver(self.Y, self.X, self.lmbd,
-                                      maxit=self.maxit, tol=self.tol,
+        print(n_iter)
+        max_iter = n_iter + 1
+        lmbd = self.lmbd / self.lmbd_max * 100
+        W, as_, _ = mixed_norm_solver(self.Y, self.X, lmbd,
+                                      maxit=max_iter, tol=self.tol,
                                       n_orient=self.n_orient, solver="bcd",
                                       verbose=0)
         self.W = build_full_coefficient_matrix(as_, self.Y.shape[1], W)
