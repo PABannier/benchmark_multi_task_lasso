c1a9c1dca5a0dd6f52ec93956e6daf1c362f0055
Pierre-Antoine Bannier <pierre-antoine@mbpdepieantoine.home>
2021-07-20 15:14:11 +0200

    WIP getting there

--- solvers/bcd_mne.py
+++ solvers/bcd_mne.py
@@ -3,6 +3,7 @@ from benchopt import BaseSolver
 from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
+    import numpy as np
     from mne.inverse_sparse.mxne_inverse import mixed_norm_solver
     from mtl_utils.common import build_full_coefficient_matrix, sum_squared
 
@@ -12,22 +13,20 @@ class Solver(BaseSolver):
     stop_strategy = "iteration"
 
     def set_objective(self, X, Y, lmbd, n_orient):
-        self.X, self.Y = X.copy(), Y
+        self.X, self.Y = X, Y
         self.n_orient = n_orient
-        self.tol = 1e-12 * sum_squared(self.Y)
-
-        # Rescale alpha to be in [0, 100)
-        self.lmbd_max = norm_l2inf(X.T @ Y, n_orient, copy=False) * 0.01
-        self.reg = lmbd / (self.lmbd_max * 100)
-        self.X /= self.lmbd_max
+        self.tol = 1e-12
+        self.lmbd = lmbd
 
     def run(self, n_iter):
-        max_iter = n_iter + 1
-        W_, as_, _ = mixed_norm_solver(self.Y, self.X, self.reg * 100,
-                                       maxit=max_iter, tol=self.tol,
-                                       n_orient=self.n_orient, solver="bcd",
-                                       verbose=0)
-        self.W = build_full_coefficient_matrix(as_, self.Y.shape[1], W_)
+        if n_iter == 0:
+            self.W = np.zeros((self.X.shape[1], self.Y.shape[1]))
+        else:
+            W_, as_, _ = mixed_norm_solver(self.Y, self.X, self.lmbd,
+                                           maxit=n_iter, tol=self.tol,
+                                           n_orient=self.n_orient,
+                                           solver="bcd", verbose=0)
+            self.W = build_full_coefficient_matrix(as_, self.Y.shape[1], W_)
 
     def get_result(self):
         return self.W
