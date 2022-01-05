e3753e5a3837b38ad9d89775127537d35344af7d
mathurinm <mathurin.massias@gmail.com>
2022-01-05 12:54:49 +0100

    skip celer_cython

--- xfail.py
+++ xfail.py
@@ -10,4 +10,8 @@ def xfail_test_solver_install(solver_class):
     particular architecture, call pytest.xfail when
     detecting the situation.
     """
-    pass
+    if solver_class == "celer_cython":
+        pytest.xfail(
+            "Celer's install on CI raises: ValueError: numpy.ndarray size "
+            "changed, may indicate binary incompatibility. Expected 96 from C "
+            "header, got 88 from PyObject")
